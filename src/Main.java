295927f18ef4fbf33eb7d23346961cba44c50275
Juliaspr <juliyaspr@gmail.com>
2021-03-11 21:41:44 +0300

    Task Two Complete

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,6 @@ import DynamicArray.DynamicArray;
 public class Main {
 
     public static void main(String[] args) {
-        DynamicArray<Integer> array = new DynamicArray<Integer>(1);
-
-        array.insert(0, 111);
-        array.insert(1, 111);
-        array.insert(2, 222);
-        array.insert(3, 111);
-        array.insert(4, 111);
-        array.printAll();
-        System.out.println();
-
-        array.remove(2);
-        array.printAll();
+        // done
     }
 }
