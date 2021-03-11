01ed592caaaa9775ac56b819d9fec26b5de35028
Juliaspr <juliyaspr@gmail.com>
2021-03-11 18:55:32 +0300

    resize-printALl

--- src/DynamicArray/DynamicArray.java
+++ src/DynamicArray/DynamicArray.java
@@ -1,10 +1,13 @@
 package DynamicArray;
 
+import java.util.Arrays;
+
 public class DynamicArray<T> {
     T[] array;
-    int SIZE_DEFAULT = 0;
+    int SIZE_DEFAULT = 1;
 
     String error_outOfBounds = "the array is out of bounds";
+    String error_wrongSize = "the array size cannot be less than 1";
 
     public DynamicArray() {
         array = (T[]) new Object[SIZE_DEFAULT];
@@ -20,6 +23,25 @@ public class DynamicArray<T> {
         }
 
         array[index] = data;
+    }
+
+    public void resize(int newSize){
+        if (newSize < 1) {
+            throw new RuntimeException(error_wrongSize);
+        }
+
+        if (newSize < array.length) {
+            array = Arrays.copyOf(array, newSize);
+            return;
+        }
+
+        array = Arrays.copyOf(array, array.length + newSize);
+    }
+
+    public void printAll() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import DynamicArray.DynamicArray;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DynamicArray array = new DynamicArray(10);
+
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+
+        array.printAll();
+
+        System.out.println();
+
+        array.resize(4);
+        array.printAll();
+
+        System.out.println();
+
+        array.resize(10);
+        array.printAll();
+
     }
 }
