78a3ab61f66a239ff260c173bb4b654638498b99
Juliaspr <juliyaspr@gmail.com>
2021-03-17 14:44:33 +0300

    DoubleLinkedList Complete

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,6 @@ import DoublyLinkedList.*;
 public class Main {
 
     public static void main(String[] args) {
-        DoublyLinkedList<Integer> list = new DoublyLinkedList<Integer>();
-
-        list.append(1);
-        list.append(2);
-        list.append(3);
-        list.append(4);
-        list.append(5);
-        list.append(6);
-        list.printAll();
-
-
-        Element<Integer> node = list.getElement(5);
-        Element<Integer> newElement = new Element<Integer>(4444);
-        list.insertAfter(node, newElement);
-        list.printAll();
 
     }
 }
