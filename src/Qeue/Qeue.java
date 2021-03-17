9cf6d9bdaa7316f1342524d992ccd16bb07f1c29
Juliaspr <juliyaspr@gmail.com>
2021-03-17 14:53:42 +0300

    enqeue

--- src/Qeue/Qeue.java
+++ src/Qeue/Qeue.java
@@ -5,4 +5,11 @@ import DynamicArray.DynamicArray;
 public class Qeue<T> {
     int size = 0;
     DynamicArray<T> array = new DynamicArray<T>(size);
+
+    public void enqeue(T data) {
+        array.add(data);
+        size++;
+    }
+
+
 }
