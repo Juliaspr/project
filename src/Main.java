780fd1d807dd9497c90629bdd19d51eb2a98e235
Juliaspr <juliyaspr@gmail.com>
2021-03-25 16:56:53 +0300

    Task 6 complete

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,6 @@ import Qeue.*;
 public class Main {
 
     public static void main(String[] args) {
-        Deck<Integer> deck = new Deck<Integer>();
-
-        deck.pushBack(1);
-        deck.pushBack(2);
-        deck.pushBack(3);
-        deck.printAll();
-        System.out.println();
-
-        deck.pushFront(222);
-        deck.printAll();
-        System.out.println();
-
-        deck.popBack();
-        deck.popBack();
-        deck.printAll();
-        System.out.println();
-
-        System.out.println(deck.peekBack());
+        // Task 6 complete
     }
 }
