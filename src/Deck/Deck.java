bb39b59f9e396534574403caf555e0c81c21aba5
Juliaspr <juliyaspr@gmail.com>
2021-03-25 16:45:44 +0300

    tests

--- src/Deck/Deck.java
+++ src/Deck/Deck.java
@@ -4,7 +4,10 @@ import DoublyLinkedList.*;
 import DynamicArray.DynamicArray;
 
 public class Deck<T> {
-    DynamicArray<T> array = new DynamicArray<T>();
+    private String deckIsEmptyError = "there's nothing in deck. It's empty";
+    private DynamicArray<T> array = new DynamicArray<T>();
+
+
 
     public void pushFront(T value) {
         array.addFront(value);
@@ -16,5 +19,21 @@ public class Deck<T> {
 
     public T peekFront() {
         return array.get(0);
+    }
+
+    public int getSize() {
+        return array.getSize();
+    }
+
+    public void printAll() {
+        array.printAll();
+    }
+
+    public String get_errorMessage(String error) {
+        if (error.equals(deckIsEmptyError)) {
+            return deckIsEmptyError;
+        }
+
+        return "No error message";
     }
 }
--- src/Deck/DeckTests.java
+++ src/Deck/DeckTests.java
@@ -1,4 +1,78 @@
 package Deck;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DeckTests {
+
+    @Test
+    public void deck_init() {
+        Deck<Integer> deck = new Deck<Integer>();
+    }
+
+    @Test
+    public void getSize_emptyArray() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        int expected = 0;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getSize_NotEmptyArray() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(0);
+        deck.pushFront(0);
+
+        int expected = 2;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void peekFront_EmptyArray() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        String error_message = deck.get_errorMessage("deckIsEmptyError");
+
+        assertThrows(RuntimeException.class, deck::peekFront, error_message);
+    }
+
+    @Test
+    public void peekFront_NotEmptyArray() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(1);
+        deck.pushFront(0);
+
+        int expected = 0;
+        int actual = deck.peekFront();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void pushFront_sizeValidation() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(0);
+        deck.pushFront(0);
+
+        int expected = 2;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void pushFront_dataValidation() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(1);
+        deck.pushFront(2);
+
+        int expected = 2;
+        int actual = deck.peekFront();
+        assertEquals(expected, actual);
+    }
 }
