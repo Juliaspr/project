1aff16858960f54a63759d4c3e48aac50f9181e7
Juliaspr <juliyaspr@gmail.com>
2021-05-20 09:34:59 +0300

    class initiation + test

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -2,7 +2,20 @@ package HashTable;
 import DoublyLinkedList.*;
 
 public class HashTable {
-    int[] item = new int[1]; // 0 - key; 1 - data;
-    int size = 5;
+    // each one of the table object is an array
+    // object[0] - key of the object
+    // object[1] - data of the object
+
+    int size;
     DoublyLinkedList[] data;
+
+    public HashTable() {
+        size = 5;
+        data = new DoublyLinkedList[size];
+    }
+
+    public HashTable(int size) {
+        this.size = size;
+        data = new DoublyLinkedList[size];
+    }
 }
--- src/HashTable/HashTableTests.java
+++ src/HashTable/HashTableTests.java
@@ -1,4 +1,11 @@
 package HashTable;
 
+import org.junit.jupiter.api.Test;
+
 public class HashTableTests {
+
+    @Test
+    public void init_test() {
+        HashTable table = new HashTable();
+    }
 }
