a2eca4b579395e64d8f4797a97f484744d0d8b69
juliaspr <juliaspr@gmail.com>
2021-06-12 19:43:22 +0300

    init

--- src/Graphs/Graphs.java
+++ src/Graphs/Graphs.java
@@ -1,4 +1,16 @@
 package Graphs;
+import java.util.ArrayList;
 
 public class Graphs {
+    ArrayList<ArrayList<Integer>> graph;
+    int node;
+
+    public Graphs(int nodes) {
+        this.node = nodes;
+        graph = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < nodes; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+    }
 }
