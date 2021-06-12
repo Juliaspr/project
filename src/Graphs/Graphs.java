9499e9d9812b448b7a19456c2c5b936ad5b79144
juliaspr <juliaspr@gmail.com>
2021-06-12 19:49:52 +0300

    get

--- src/Graphs/Graphs.java
+++ src/Graphs/Graphs.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 
 public class Graphs {
     ArrayList<ArrayList<Integer>> graph;
-    int node;
+    int nodes;
 
     public Graphs(int nodes) {
-        this.node = nodes;
+        this.nodes = nodes;
         graph = new ArrayList<ArrayList<Integer>>();
 
         for (int i = 0; i < nodes; i++) {
             graph.add(new ArrayList<Integer>());
         }
     }
+
+    public ArrayList<Integer> get(int id) {
+        return graph.get(id);
+    }
+
 }
