f3dabf8cfb2d50cd675ae77c1c33d44ac480d742
juliaspr <juliaspr@gmail.com>
2021-06-12 20:00:03 +0300

    delete

--- src/Graphs/Graphs.java
+++ src/Graphs/Graphs.java
@@ -18,4 +18,23 @@ public class Graphs {
         return graph.get(id);
     }
 
+    public void delete(int source, int destination)
+    {
+        for (int i = 0; i < graph.get(destination).size(); i++)
+        {
+            if (graph.get(destination).get(i).equals(source))
+            {
+                graph.get(destination).remove(i);
+                break;
+            }
+        }
+        for (int i = 0; i < graph.get(source).size(); i++)
+        {
+            if (graph.get(source).get(i) == destination)
+            {
+                graph.get(source).remove(i);
+                break;
+            }
+        }
+    }
 }
