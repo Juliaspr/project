57e2253de5f8381be777f361e7d09769fd3a8159
juliaspr <juliaspr@gmail.com>
2021-06-15 11:35:42 +0300

    C3 additional complete

--- src/Graphs/GraphsTest.java
+++ src/Graphs/GraphsTest.java
@@ -22,4 +22,18 @@ public class GraphsTest {
         int actual = graph.get(3).get(1);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void get_returns() {
+        Graphs graph = new Graphs(10);
+        graph.add(4, 2);
+        graph.add(3, 1);
+        graph.add(2, 3);
+        graph.add(1, 1);
+        graph.delete(2, 1);
+
+        int expected = 2;
+        int actual = graph.get(3).get(1);
+        assertEquals(expected, actual);
+    }
 }
